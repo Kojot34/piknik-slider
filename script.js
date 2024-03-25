37a6032271eb01399799242c8bc6f6d0291cbe83
Kojot34 <53234651+Kojot34@users.noreply.github.com>
2024-03-25 14:05:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,61 @@
+var middleDiv = document.createElement('div');
+middleDiv.classList.add('middle');
+
+var multiRangeSliderDiv = document.createElement('div');
+multiRangeSliderDiv.classList.add('multi-range-slider');
+
+var inputLeft = document.createElement('input');
+inputLeft.type = 'range';
+inputLeft.id = 'input-left';
+inputLeft.min = '0';
+inputLeft.max = '100';
+inputLeft.value = '25';
+
+var inputRight = document.createElement('input');
+inputRight.type = 'range';
+inputRight.id = 'input-right';
+inputRight.min = '0';
+inputRight.max = '100';
+inputRight.value = '75';
+
+var sliderDiv = document.createElement('div');
+sliderDiv.classList.add('slider');
+
+var trackDiv = document.createElement('div');
+trackDiv.classList.add('track');
+
+var rangeDiv = document.createElement('div');
+rangeDiv.classList.add('range');
+
+var thumbLeftDiv = document.createElement('div');
+thumbLeftDiv.classList.add('thumb', 'left');
+
+var dotLeftDiv = document.createElement('div');
+dotLeftDiv.classList.add('dot');
+thumbLeftDiv.appendChild(dotLeftDiv);
+
+var thumbRightDiv = document.createElement('div');
+thumbRightDiv.classList.add('thumb', 'right');
+
+var dotRightDiv = document.createElement('div');
+dotRightDiv.classList.add('dot');
+thumbRightDiv.appendChild(dotRightDiv);
+
+// Dodawanie elementów do drzewa DOM
+sliderDiv.appendChild(trackDiv);
+sliderDiv.appendChild(rangeDiv);
+sliderDiv.appendChild(thumbLeftDiv);
+sliderDiv.appendChild(thumbRightDiv);
+
+multiRangeSliderDiv.appendChild(inputLeft);
+multiRangeSliderDiv.appendChild(inputRight);
+multiRangeSliderDiv.appendChild(sliderDiv);
+
+middleDiv.appendChild(multiRangeSliderDiv);
+
+
+
+//script
 var inputLeft = document.getElementById("input-left");
 var inputRight = document.getElementById("input-right");
 
